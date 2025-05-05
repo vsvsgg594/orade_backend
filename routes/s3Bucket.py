81abeadaa84dd54a38532deef6a81fdc3f17a840
vsvsgg594 <vsvsgg594@gmail.com>
2025-05-05 12:07:34 +0530

    added updated connedcdt

--- routes/s3Bucket.py
+++ routes/s3Bucket.py
@@ -11,27 +11,27 @@ logging.basicConfig(level=logging.INFO)
 
 # s3 bucket subfunctions
 
-AWS_BUCKET = os.getenv('AWS_BUCKET')
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+# AWS_BUCKET = os.getenv('AWS_BUCKET')
+# AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 
-s3 = boto3.resource('s3',
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-bucket = s3.Bucket(AWS_BUCKET)
+# s3 = boto3.resource('s3',
+#     aws_access_key_id=AWS_ACCESS_KEY_ID,
+#     aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+# bucket = s3.Bucket(AWS_BUCKET)
 
-def s3Upload(contents: bytes, key: str):
-    logging.info(f'Uploading {key} to s3')
-    bucket.put_object(Key=key, Body=contents)
+# def s3Upload(contents: bytes, key: str):
+#     logging.info(f'Uploading {key} to s3')
+#     bucket.put_object(Key=key, Body=contents)
 
-s4 = boto3.client('s3',
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+# s4 = boto3.client('s3',
+#     aws_access_key_id=AWS_ACCESS_KEY_ID,
+#     aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-def s3Download(key: str):
-    try:
-        return s3.Object(bucket_name=AWS_BUCKET, key= key).get()['Body'].read()
-    except ClientError as err:
-        logging.error(str(err))
+# def s3Download(key: str):
+#     try:
+#         return s3.Object(bucket_name=AWS_BUCKET, key= key).get()['Body'].read()
+#     except ClientError as err:
+#         logging.error(str(err))
 
